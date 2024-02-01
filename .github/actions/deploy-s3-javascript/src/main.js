d701a8a8e22edd2c03496ccd909941b029bbf91e
Denis Bogdanenko <shiirochi@yandex.ru>
2024-02-01 23:24:31 +0300

    upload to s3 using reusable javascript action

--- .github/actions/deploy-s3-javascript/src/main.js
+++ .github/actions/deploy-s3-javascript/src/main.js
@@ -1,8 +1,20 @@
 const core = require('@actions/core');
-const githun = require('@actions/github');
+// const githun = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // get some inputs
+    const bucket$ = core.getInput('bucket', { required: true });
+    const bucketRegion$ = core.getInput('bucket-region', { required: true });
+    const dist$ = core.getInput('dist-folder', { required: true });
+
+
+
+    // upload to S3
+    const s3URI = `s3://${bucket$}`;
+    
+    exec.exec(`aws s3 sync ${dist$} ${s3URI} --region ${bucketRegion$} --endpoint-url=https://storage.yandexcloud.net`)
+
     core.notice("Hello world from my custom JavaScript Action!");
 }
 
